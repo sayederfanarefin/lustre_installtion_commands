2c14af282e5bccf8428ef416b9fa2ecc276a3648
Sayed Erfan Arefin <erfanjordison@gmail.com>
2021-04-19 17:18:45 -0500

    testing with server

--- server_commands.sh
+++ server_commands.sh
@@ -1,9 +1,7 @@
 #https://wiki.lustre.org/Installing_the_Lustre_Software
 
-yum --nogpgcheck --disablerepo=* --enablerepo=e2fsprogs-wc \
-install e2fsprogs
+yum --nogpgcheck --disablerepo=* --enablerepo=e2fsprogs-wc install e2fsprogs -y
 
-yum -y install epel-release
+yum -y install epel-release -y
 
-yum -y install \
-http://download.zfsonlinux.org/epel/zfs-release.el7_4.noarch.rpm
+yum -y install http://download.zfsonlinux.org/epel/zfs-release.el7_4.noarch.rpm -y
